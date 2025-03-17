62d7de926b7ef898dd038b3feb9d1904d0022a58
matteovalerio <matteovalerio2006@hotmail.it>
2025-03-17 22:36:18 +0100

    feat: basic todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Todo = {
+  name: string;
+  completed: boolean;
+};
+
+export function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col gap-4 w-full h-full">
+        <div className="flex items-center gap-4">
+          <div className="font-semibold text-xl">TODO list</div>
+          <div className="ml-auto" />
+          <button
+              type="button"
+              onClick={() => setTodos((prev) => [...prev, { name: "", completed: false }])}
+          >
+            Add
+          </button>
+        </div>
+        {todos.map((todo, index) => (
+            <TodoItem
+                key={index}
+                todo={todo}
+                onChange={(updatedTodo) =>
+                    setTodos((prev) => prev.map((t, i) => (i === index ? updatedTodo : t)))
+                }
+                onDelete={() => setTodos((prev) => prev.filter((_, i) => i !== index))}
+            />
+        ))}
+        <button type="button" onClick={() => console.log(todos)}>Save</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+function TodoItem({ todo, onChange, onDelete }: { todo: Todo; onChange: (todo: Todo) => void; onDelete: () => void }) {
+  return (
+      <div className="flex flex-col gap-2 border border-gray-200 p-2 rounded-lg">
+        <div className="flex gap-2 items-center">
+          <label htmlFor="name">Name</label>
+          <input
+              type="text"
+              required
+              placeholder="Enter task name..."
+              value={todo.name}
+              onChange={(e) => onChange({ ...todo, name: e.target.value })}
+              name="name"
+          />
+        </div>
+        <div className="flex gap-2 items-center">
+          <label htmlFor="isCompleted">Is completed</label>
+          <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => onChange({ ...todo, completed: !todo.completed })}
+              name="completed"
+          />
+        </div>
+        <div className="flex gap-2 items-center">
+          <div className="ml-auto" />
+          <button type="button" onClick={onDelete} className="bg-red-500 text-white p-1 rounded">
+            Delete
+          </button>
+        </div>
+      </div>
+  );
+}
